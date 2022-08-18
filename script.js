31680d0a6db8a8e2184563f332d3090b0767474b
sam conteh <sambujangconteh4@gmail.com>
2022-08-18 18:30:05 -0400

    add arrays

--- script.js
+++ script.js
@@ -1,67 +1,30 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
+// Assignment code here
 
-var lowerCaseCharacter = "abcdefghijklmnopqrstuvwxyz";
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate')
 
-var upperCaseCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var numericCharacter = "0123456789";
-
-var specialCharacter = "!@#$%^&*()_-+={[}]|:;<>.?/";
-
-let everyCharacter = "";
-
+// Function to generate password
 function generatePassword() {
-  everyCharacter = "";
-  let passwordLength = prompt(
-    "Please be mindfull that the password must be at least 8 characters and no more than 128 characters, How long do you want It to be?"
-  );
-}
-  if (
-    passwordLength < 8 ||
-    passwordLength > 128 ||
-    isNaN(parseInt(passwordLength))
-  ) {
-    alert(
-      "You haven't entered the right lenght that you want your password to be, Please enter a number between 8 and 128."
-    );
-  } else {
-    let askLowerCase = confirm(
-      "Would you like to have Lower Case characters on your password ?"
-    );
+
+  // Created array of each character type
+  var lowercaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numberSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialcharSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ";", ":", "[", "]", "{", "}", ",", ".", "<", ">", "=", "_", "`", "~", "|"];
+
+  // Created empty arrays
+  var resultArray = [];
+  var userArray = [];
+  
+
+  // Ask how many characters for password
+  function charLength() {
+    passwordLength = prompt("How many characters would you like your password? Choose a value between 8 and 128.");
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
+      // If user inputs value outside range, error message will appear
+      alert("Error! Please input a value that is between 8 and 128.");
+      charLength();
+    }
   }
-    if (askLowerCase) {
-      everyCharacter += lowerCaseCharacter;
-    }
-    let askUpperCase = confirm(
-      "Would you like to have Upper Case characters on your password ?"
-    );
-    if (askUpperCase) {
-      everyCharacter += upperCaseCharacter;
-    }
-    let askNumeric = confirm(
-      "Would you like to have Numbers on your password ?"
-    );
-    if (askNumeric) {
-      everyCharacter += numericCharacter;
-    }
-    let askSpecialCharacters = confirm(
-      "Would you like to have Special Characters on your password?"
-    );
-    if (askSpecialCharacters) {
-      everyCharacter += specialCharacter;
-    }
- 
+  charLength()
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
